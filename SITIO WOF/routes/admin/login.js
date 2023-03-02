f0d8ff84602465e92d38f2bf011dcfc6cac15404
Ana Laura Ugo <analauraugo@gmail.com>
2023-03-01 21:13:55 -0300

    integracion node + bs - error en ruta /admin/novedades

--- SITIO WOF/routes/admin/login.js
+++ SITIO WOF/routes/admin/login.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var usuariosModel = require('./../../models/usuariosModel');
 
-router.get('/', function(req, res, next) {
-    res.render('admin/login',{
-        layout:'admin/layout'
+router.get('/', function (req, res, next) {
+    res.render('admin/login', {
+        layout: 'admin/layout',
     });
-  });
+});
 
 //   para destruir variables de session
 router.get('/logout', function (req, res, next) {
@@ -16,29 +16,30 @@ router.get('/logout', function (req, res, next) {
     });
 });
 
-  router.post('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
     try {
+        //para ver si se comunica el .hbs con el .js
         console.log(req.body);
+        //para capturar los datos
         var usuario = req.body.usuario;
         var password = req.body.password;
 
-       
+
         var data = await usuariosModel.getUserAndPassword(usuario, password);
 
-    if (data != undefined) {
-        req.session.id_usuario = data.id;
-        req.session.nombre = data.usuario;
-        
-        res.redirect('/admin/novedades');
-          } else {
-        res.render('admin/login', {
-            layout: 'admin/layout',
-            error: true
-        })
-    } //cierre else
+        if (data != undefined) {
+            req.session.id_usuario = data.id; //nombre de la columna
+            req.session.nombre = data.usuario; //usuario también es columna de mi BDS   
+            res.redirect('admin/novedades');
+        } else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            })
+        } //cierre else
     } catch (error) {
         console.log(error);
     }
-  })
+})
 
-  module.exports = router;
+module.exports = router;
--- SITIO WOF/routes/index.js
+++ SITIO WOF/routes/index.js
@@ -9,12 +9,11 @@ router.get('/', function(req, res, next) {
 
 router.post("/", async(req, res, next) => {
 
-  console.log(req.body) //saber si estoy tomando bien los datos
+  console.log(req.body); //saber si estoy tomando bien los datos
 
   var mascota = req.body.mascota;
   var sexo = req.body.sexo;
   var nombremascota = req.body.nombremascota;
-  var adjunto = req.body.adjunto;
   var fecha = req.body.fecha;
   var nombre = req.body.nombre;
   var email = req.body.email;
@@ -25,14 +24,14 @@ router.post("/", async(req, res, next) => {
     to: 'analauraugo@gmail.com',
     subject: 'Contacto desde la Web' ,
     html: nombre + " se contacto a través de la web y quiere notificar que perdió a su : " + mascota + " se trata de un/una " + sexo + " llamado/a: " + nombremascota + " Se perdió el día: " + fecha + " Su mail es: " + email + " Su celular: " + celular
-  }
+  };
 
   var transport = nodemailer.createTransport ({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     auth: {
       user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
+      pass: process.env.SMTP_PASS,
     }
   });
 
