6ae6a625afb5e8a98fedc98f4b1ad1d485da9531
Ana Laura Ugo <analauraugo@gmail.com>
2023-03-15 19:27:17 -0300

    CRUD Parte 3

--- SITIO WOF/routes/admin/novedades.js
+++ SITIO WOF/routes/admin/novedades.js
@@ -3,13 +3,37 @@ var router = express.Router();
 
 var novedadesModel = require('../../models/novedadesModel');
 
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 
-// GET novedades page
+
+//listar las novedades
 router.get('/', async function (req, res, next) {
 
 
   var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        width: 50,
+        height: 50,
+        crop: 'fill'
+      });
+      return {
+        ...novedad, //titulo, subtitulo y cuerpo
+        imagen //imagen
+      }
+    }else {
+      return {
+        ...novedad, //titulo, subtitulo y cuerpo
+        imagen: '' //aca puedo poner una imagen que diga no disponible y poner la ruta para que se vea
+      }
+    }
+  });
 
 
   res.render('admin/novedades', {
@@ -20,7 +44,13 @@ router.get('/', async function (req, res, next) {
 
 /* para eliminar una novedad*/
 router.get('/eliminar/:id', async (req, res, next) => {
-  const id = req.params.id;
+  var id = req.params.id;
+
+let novedad = await novedadesModel.getNovedadById(id);
+if (novedad.img_id) {
+  await (destroy(novedad.img_id));
+}
+
   await novedadesModel.deleteNovedadesById(id);
   res.redirect('/admin/novedades')
 
@@ -39,8 +69,20 @@ router.get('/agregar', (req, res, next) => {
 
 router.post('/agregar', async (req, res, next) => {
   try {
+
+    var img_id = '';
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
+
+    //console.log(req.body)
     if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
-      await novedadesModel.insertNovedad(req.body);
+      await novedadesModel.insertNovedad({
+        ...req.body, //spread titulo,sub y cuerpo
+        img_id
+      });
       res.redirect('/admin/novedades')
     } else {
       res.render('admin/agregar', {
@@ -72,14 +114,33 @@ router.get('/modificar/:id', async (req, res, next) => {
 // actualizar la novedad
 router.post('/modificar', async (req, res, next) => {
   try {
-    console.log(req.body.id); //para ver si tra id
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+    
+    if (req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await (destroy(req.body.img_original));
+    }
+
+
+    //console.log(req.body.id); //para ver si trae id
     var obj = {
       titulo: req.body.titulo,
       subtitulo: req.body.subtitulo,
-      cuerpo: req.body.cuerpo
+      cuerpo: req.body.cuerpo,
+      img_id
     }
 
-    console.log(obj) //para ver si trae los datos
+    //console.log(obj) //para ver si trae los datos
     await novedadesModel.modificarNovedadById(obj, req.body.id);
     res.redirect('/admin/novedades');
 
